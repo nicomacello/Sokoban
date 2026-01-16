640d9ed97387bd94ea74f5affdd8aca9b15859f7
AxlPannaCotta <niccolo.macello@gmail.com>
2026-01-16 15:33:08 +0100

    SavingPush

--- Sokoban/Assets/Scripts/PushableBlock.cs
+++ Sokoban/Assets/Scripts/PushableBlock.cs
@@ -1,16 +1,66 @@
 using UnityEngine;
+using System.Collections;
 
 public class PushableBlock : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float gridSize = 1f;
+    [SerializeField] private LayerMask obstacleLayer;
+
+    private bool isMoving = false;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (isMoving) return;
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Calcoliamo la direzione dal Player verso il Blocco
+            Vector3 dir = transform.position - collision.transform.position;
+            
+            // Rendiamo la direzione secca (solo X o solo Z)
+            Vector3 moveDir = Vector3.zero;
+            if (Mathf.Abs(dir.x) > Mathf.Abs(dir.z))
+                moveDir = new Vector3(Mathf.Sign(dir.x), 0, 0);
+            else
+                moveDir = new Vector3(0, 0, Mathf.Sign(dir.z));
+
+            Debug.Log("Tentativo di movimento in direzione: " + moveDir);
+            TryMove(moveDir);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TryMove(Vector3 direction)
     {
-        
+        Vector3 targetPos = transform.position + direction * gridSize;
+
+        // Visualizza il raggio nel Scene View per debug
+        Debug.DrawRay(transform.position, direction * gridSize, Color.red, 1f);
+
+        if (!Physics.Raycast(transform.position, direction, gridSize, obstacleLayer))
+        {
+            StartCoroutine(SmoothMove(targetPos));
+        }
+        else
+        {
+            Debug.Log("Movimento bloccato da un ostacolo!");
+        }
+    }
+
+    private IEnumerator SmoothMove(Vector3 target)
+    {
+        isMoving = true;
+        Vector3 startPos = transform.position;
+        float percent = 0;
+
+        while (percent < 1f)
+        {
+            percent += Time.deltaTime * moveSpeed;
+            transform.position = Vector3.Lerp(startPos, target, percent);
+            yield return null;
+        }
+
+        transform.position = target;
+        isMoving = false;
     }
 }

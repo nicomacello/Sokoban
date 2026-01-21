144b874aa3e64d8b6cd603ea74bd99a04e17a759
AxlPannaCotta <niccolo.macello@gmail.com>
2026-01-21 16:07:14 +0100

    BU

---
+++ Sokoban/Assets/Scripts/WinDoor.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class WinDoor : MonoBehaviour
+{
+    [SerializeField]private GameObject winPanel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FinishGame();
+        }
+    }
+
+    private void FinishGame()
+    {
+        if (winPanel != null)
+        {
+            winPanel.SetActive(true);
+        }
+
+        Time.timeScale = 0f;
+
+        Debug.Log("Vittoria Raggiunta!");
+    }
+}
